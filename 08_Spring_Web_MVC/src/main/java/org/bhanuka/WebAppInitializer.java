2efdccb9112c6ccff2fd16a12df901136b7d1fba
bhanuka-viraj <145824067+bhanuka-viraj@users.noreply.github.com>
2025-06-26 14:56:36 +0530

    06-26-2025_2:56pm

--- 08_Spring_Web_MVC/src/main/java/org/bhanuka/WebAppInitializer.java
+++ 08_Spring_Web_MVC/src/main/java/org/bhanuka/WebAppInitializer.java
@@ -7,11 +7,13 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 
 public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
+    //adding the webRootConfig to the root context
     @Override
     protected Class<?> @Nullable [] getRootConfigClasses() {
         return new Class[]{WebRootConfig.class};
     }
 
+    // adding the webAppConfig to the servlet context
     @Override
     protected Class<?> @Nullable [] getServletConfigClasses() {
         return new Class[]{WebAppConfig.class};
@@ -23,9 +25,27 @@ public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServlet
     }
 
 
-    // install tomcat,
-    // in intellij > go to project structure > create web facet for the module > create artifact and add the libraries
-    // add the configuration tomcat local
+    // 1. install tomcat,
+    // 2. in intellij > go to project structure > create web facet for the module > create artifact and add the libraries
+    // 3. add the configuration tomcat local
 
     //** add servlet api dependency
+
+
+
+    /*
+           ---------- contexts -----------
+    * servlet web app context  = controllers , View controllers, Handler Mappings (also known as child context)
+    * root web context = services and repositories (also known as parent context)
+    */
+
+
+    /*
+            ---------- DispatcherServlet -----------
+
+        DispatcherServlet acts as a front controller, handling all incoming HTTP requests and routing them to
+        the appropriate Spring MVC controllers. It's the central component for managing the request-response
+        lifecycle, essentially directing traffic within the application
+     */
+
 }
--- 08_Spring_Web_MVC/src/main/java/org/bhanuka/controller/HelloController.java
+++ 08_Spring_Web_MVC/src/main/java/org/bhanuka/controller/HelloController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//@Controller       //** can also return views, if the views are also in the project
 @RequestMapping("/hello")
 public class HelloController {
     public HelloController(){
@@ -14,5 +15,15 @@ public class HelloController {
     @GetMapping
     public String hello(){
         return "Hello World";
+    }
+
+    @GetMapping("two")
+    public String hello2(){
+        return "Hello World2";
+    }
+
+    @GetMapping("three")
+    public String hello3(){
+        return "Hello World3";
     }
 }
